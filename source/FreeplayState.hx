6ce2299c3dccaf6a40b705eea3081fc5518b6436
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-12-07 19:36:10 +0800

    Update FreeplayState.hx

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -59,9 +59,6 @@ class FreeplayState extends MusicBeatState
 
 	override function create()
 	{
-		Paths.clearStoredMemory();
-		Paths.clearUnusedMemory();
-		
 		persistentUpdate = true;
 		PlayState.isStoryMode = false;
 		WeekData.reloadWeekFiles(false);
